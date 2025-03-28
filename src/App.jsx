fa3b385f87e1864107ca09312eb687c6237d4e1b
Victor Otene <solomonotene36@gmail.com>
2025-03-28 13:12:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="App text-3xl font-bold text-red-300 underline">
+        <h1>Welcome to React</h1>
+
+       </div>
+    </>
+  )
+}
+
+export default App
